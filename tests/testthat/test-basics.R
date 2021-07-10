d0bea2b19bb0ff4db6325286559e97b505f503fb
Matt Stern <mcpstern@pm.me>
2021-07-10 16:09:01 -0500

    test code

--- tests/testthat/test-basics.R
+++ tests/testthat/test-basics.R
@@ -20,3 +20,35 @@ test_that("Filter recessions works: dates", {
   expect_equal(df[[10,2]], lubridate::ymd(20090201))
 })
 
+
+test_that("Current test code", {
+  library(tidyverse)
+  df <- data.frame(year_dec=1950:1999, value=rnorm(100), var=c(rep("A", 50), rep("B", 50)))
+  df$year_date <- as.Date(lubridate::date_decimal(df$year_dec))
+
+  devtools::document()
+  devtools::load_all()
+
+
+  ggplot(df, mapping = aes(x = year_date, y = value)) +
+    geom_line(aes(color = var)) +
+    geom_rect(aes(xmin = after_stat(start),
+                  xmax = after_stat(end),
+                  ymin = after_stat(-1),
+                  ymax = after_stat(1)),
+              from_data = FALSE,
+              show_ongoing = FALSE,
+              stat = "recessions") +
+    geom_text(mapping = aes(x = after_stat(end)),
+              label = "recession",
+              stat = "recessions",
+              y = 2,
+              angle = 270,
+              vjust = "bottom", # actually horiz, bc rotation
+              hjust = "left", # actually vertical, bc rotation
+              from_data = FALSE,
+              show_ongoing = FALSE) +
+    scale_x_date("Year") +
+    theme_minimal()
+
+})
